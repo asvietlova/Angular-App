2a17fc73992085497119d2272c7dccd8693df1c4
Anastasiia Svietlova <anastasiiasvietlova@gmail.com>
2022-10-17 00:08:22 +0200

    Added router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,6 @@ import { ModalService } from './services/modal.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'angular-crash-course-2022';
-  loading = false
-  //products$: Observable<IProduct[]>
-  term = ''
-
-  constructor(
-    public productsService: ProductsService,
-    public modalService: ModalService
-  ) {
-
-  }
-
-  ngOnInit(): void {
-    this.loading = true
-    // this.products$ = this.productsService.getAll().pipe(
-    //   tap(() => this.loading = false)
-    // )
-    this.productsService.getAll().subscribe(() => {
-      this.loading = false
-    })
-    }
+export class AppComponent {
+  
   }
